0fe900ca666be340aa11bb5b624320f47241370c
Tomas Janousek <tomi@nomi.cz>
2021-01-31 21:01:11 +0000

    Persistent per-session xmonad state in $XDG_RUNTIME_DIR
    With writeStateToFile in logHook, this means I don't lose xmonad state
    when it crashes or hangs and is killed. (Useful for xmonad developers,
    less useful to ordinary users.)

--- src/XMonad/Main.hs
+++ src/XMonad/Main.hs
@@ -232,10 +232,7 @@ launch initxmc drs = do
     allocaXEvent $ \e ->
         runX cf st $ do
             -- check for serialized state in a file.
-            serializedSt <- do
-                path <- stateFileName
-                exists <- io (doesFileExist path)
-                if exists then readStateFile initxmc else return Nothing
+            serializedSt <- readStateFile initxmc
 
             -- restore extensibleState if we read it from a file.
             let extst = maybe M.empty extensibleState serializedSt
